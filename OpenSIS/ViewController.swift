21f00ed015903fb2bcf439f8c4844a5f90c307ee
RejaulDM <rejaul.dm@gmail.com>
2022-11-17 00:31:54 +0530

    Initial Commit

---
+++ OpenSIS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenSIS
+//
+//  Created by Rejaul on 11/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
